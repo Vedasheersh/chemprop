11aafc0a8466a4da7e615f79a2ce6a2b722f9943
Veda Sheersh Boorla <mailforveda@gmail.com>
2024-03-20 15:56:32 -0400

    Update predict.py
    remove duplicate code

--- predict.py
+++ predict.py
@@ -4,9 +4,3 @@ from chemprop.train import chemprop_predict
 
 if __name__ == '__main__':
     chemprop_predict()
-"""Loads a trained chemprop model checkpoint and makes predictions on a dataset."""
-
-from chemprop.train import chemprop_predict
-
-if __name__ == '__main__':
-    chemprop_predict()
