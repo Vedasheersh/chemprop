9c935b72000b3363bdf8466dc770f16e0aff1341
Veda Sheersh Boorla <mailforveda@gmail.com>
2024-03-20 19:57:53 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,9 @@
 """Loads a trained chemprop model checkpoint and makes predictions on a dataset."""
 
 from chemprop.train import chemprop_predict_and_fp
+import ipdb
 
 if __name__ == '__main__':
-    chemprop_predict_and_fp()
+    results = chemprop_predict_and_fp()
+    ipdb.set_trace()
+    
